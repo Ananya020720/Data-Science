0ef007198369095b2f875a76a7f309ed72031eb3
Ananya Shetty <83279685+Ananya020720@users.noreply.github.com>
2025-08-01 23:07:56 +0530

    Update breast_cancer_classification.py

--- breast_cancer_classification.py
+++ breast_cancer_classification.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+pip install scikit
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
